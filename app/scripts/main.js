292073ba7fdd81c7d2abc9dcafad6328f2542942
RJ Goetz <rj@rjgoetz.com>
2015-04-29 16:57:42 -0500

    added carousel plugin, removed page-header fixed position, need to readdress page-header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,3 +2,16 @@
 
 $('.main-logo').imagefill();
 $('.product-image').imagefill();
+$('.carousel-image').imagefill();
+
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayHoverPause:true,
+    loop:true,
+    items: 5,
+    autoWidth: true
+})
